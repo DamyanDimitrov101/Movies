739cbf9fd9affcc37aa8eb49d3640eacd34a1f9d
Damyan <teodorossss@abv.bg>
2020-12-29 06:42:16 +0200

    Home, Login, Register templates add
    templating

--- init.js
+++ init.js
@@ -1,5 +1,14 @@
 import {Router} from 'https://unpkg.com/@vaadin/router';
 
+
+import Home from './components/home-component.js';
+import Register from './components/register-component.js';
+import Login from './components/login-component.js';
+
+customElements.define('home-component', Home);
+customElements.define('login-component', Login);
+customElements.define('register-component', Register);
+
 const root = document.getElementById('root');
 
 
